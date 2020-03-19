a72f050970ec50dfa640f1cbb6ce6bccf32defdf
Aswin <aswinhacker4@gmail.com>
2020-03-19 10:20:47 +0530

    Delete MyProject.py

--- MyProject.py
+++
@@ -1,5 +0,0 @@
-import random,string
-a=""
-length=int(input("Enter Your Password's Length = "))
-set=string.ascii_lowercase+string.ascii_uppercase+string.digits+"!@#$%^&*(){}[]"
-print(a.join(random.choice(set) for i in range(0,length)))
